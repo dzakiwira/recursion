7038d47ee08fa5f61031c0a1f1410ed75053b52d
Dzaki Wiradinata <102267638+dzakiwira@users.noreply.github.com>
2024-09-11 00:20:24 -0400

    Initial commit

---
+++ mergeSort.js
@@ -0,0 +1,27 @@
+let array1 = [3, 2, 1, 13, 8, 5, 0, 1];
+let array2 = [105, 79, 100, 110];
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  else {
+    let sortedArray = [];
+    let arrL = [];
+    let arrR = [];
+    let midPoint = Math.floor(arr.length / 2 - 1);
+
+    arrL = mergeSort(arr.slice(0, midPoint + 1));
+    arrR = mergeSort(arr.slice(midPoint + 1));
+
+    while (arrL.length && arrR.length) {
+      if (arrL[0] < arrR[0]) {
+        sortedArray.push(arrL.shift());
+      } else {
+        sortedArray.push(arrR.shift());
+      }
+    }
+    return sortedArray.concat(arrL).concat(arrR);
+  }
+}
+
+console.log(mergeSort(array1));
+console.log(mergeSort(array2));
